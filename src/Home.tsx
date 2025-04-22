c745ca7b6b2623b27ecfe5c9c154ed143231ff6f
hikagami0210 <fsban772@gmail.com>
2025-04-22 23:38:57 +0900

    update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 export const Home: FC = () => {
   return (
     <div className="flex flex-col items-center  h-screen w-screen">
-      <h1 className="text-4xl font-bold">Home Page</h1>
+      <h1 className="text-4xl font-bold">Hello Sojiro!</h1>
     </div>
   );
 };
