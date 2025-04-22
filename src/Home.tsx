8080b52aff0dc4db0e6fc20116a5d5262c3070d9
hikagami0210 <fsban772@gmail.com>
2025-04-22 23:34:18 +0900

    Initial commit

---
+++ src/Home.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Home: FC = () => {
+  return (
+    <div className="flex flex-col items-center  h-screen w-screen">
+      <h1 className="text-4xl font-bold">Home Page</h1>
+    </div>
+  );
+};
